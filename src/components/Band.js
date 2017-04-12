0b70df30aa645c1c11c98cd9b0b81fbd67d5b2f9
github username <github email address>
2017-04-12 12:29:27 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+  constructor(){
+    super()
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+  handleDelete(event) {
+    event.preventDefault()
+    this.props.store.dispatch({type: 'DELETE_BAND', id: this.props.band.id})
+  }
   render() {
     return(
       <div>
-        band component
+        <li>{this.props.band.id}: {this.props.band.text}</li>
+        <button onClick={this.handleDelete}>Delete</button>
       </div>
     );
   }
