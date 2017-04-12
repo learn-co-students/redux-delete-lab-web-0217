4103e9dd03999c7b77dd96ef7df09f81834dc6b7
hwalborn <hwalborn@gmail.com>
2017-04-12 11:39:24 -0400

    auto loading tests, huh?

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Band from './Band'
 
 class Bands extends Component {
   render() {
+    let bandList = this.props.store.getState().bands.map((band, index) => <Band key={index} band={band}/>)
     return(
       <ul>
-        Bands Component
+        {bandList}
       </ul>
     );
   }
