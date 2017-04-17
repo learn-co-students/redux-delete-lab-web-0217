b836eefe9414d410d33baf26a65af87834b68db7
dgwp-projects <zayneabraham1@gmail.com>
2017-04-17 08:51:09 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class Band extends Component {
+  constructor(props){
+  	super(props);
+  	this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(ev) {
+    this.props.store.dispatch({
+      type: 'DELETE_BAND',
+      id: this.props.band.id
+    })
+  }
+
   render() {
     return(
-      <div>
-        band component
-      </div>
+      	<div>
+	        <li>{this.props.band.text}</li>
+	        <button onClick={() => this.handleClick()} >
+	          Delete
+	        </button>
+      	</div>
     );
   }
 };
